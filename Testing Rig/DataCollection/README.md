d7f607bf53374b96faa3d32da2d4d8cb0ac5d758
Arnav Koshy <arnav.koshy@gmail.com>
2023-06-07 23:11:49 +0100

    Cleaned up data collection and updated docs

--- Testing Rig/DataCollection/README.md
+++ Testing Rig/DataCollection/README.md
@@ -1,1 +1,35 @@
-Description of code, instructions for use
+# Data collection documentation
+Author: Arnav
+
+## Description
+---
+
+This folder contains the files necessary to collect and store data from the Arduino when running light intensity measurements.
+
+The `main.py` file does the following:
+
+    - Establishes a connection with the Arduino over Serial.
+    - Creates a new file in which to store data of a name of your choosing.
+    - Requests the Arduino for data on command.
+    - Stores the received data with an associated label.
+
+
+## Instructions to run
+---
+
+### Requirements:
+    - Python 3.10 or later
+    - Python pip
+    
+### Running
+
+Install necessary modules with `pip install -r requirements.txt`
+
+Edit `main.py lines 5-6` with the serial port and baud rate (if you've changed it) of your Arduino (To find your serial port, try [this link](https://uk.mathworks.com/help/supportpkg/arduinoio/ug/find-arduino-port-on-windows-mac-and-linux.html)).
+
+Then run `python main.py`
+
+Enter a file name to store data
+When prompted, enter a data label for the data point being collected. The python file will request data from the Arduino, wait until it responds (or times out) and then store that data under the label specified.
+
+
--- Testing Rig/DataCollection/main.py
+++ Testing Rig/DataCollection/main.py
@@ -2,8 +2,12 @@ import csv
 # changed to csv format for easier data processing
 import serial
 
+PORT = '/dev/ttyACM0'
+BAUD = 9600
+
+
 # Open the serial connection
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=25)  # Replace 'COM1' with the appropriate serial port
+ser = serial.Serial(PORT, BAUD, timeout=25)  # Replace 'COM1' with the appropriate serial port
 
 file_name = input("Enter file name: ")
 
@@ -19,16 +23,22 @@ with open(file_name, 'w+', newline='') as file:
     writer.writerow(["Test Name", "Data"])
 
     while True:
-        label = input("Input test name, or q to end:")
-        if label.strip() == "q":
-            break
-        print("Waiting for data")
-        ser.write(b's')
 
-        line = ser.readline().decode("ascii").strip()
+        # Wait for label, quit if q
+        label = input("Input test name, or q to end:")
+        label = label.strip()
+        if label == "q":
+            break
+
+        # Ping arduino and wait
+        ser.write(b's')
+        print("Waiting for data")
+
+        # Collect data and write to file
+        line = ser.readline().decode("ascii").strip().split(',')
         print(line)
 
-        writer.writerow([label, line])
+        writer.writerow([label] + [line])
 
 # Close the serial connection
 ser.close()
